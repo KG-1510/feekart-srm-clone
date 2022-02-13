2d98dd7d126470ad0d1f828a74d74cfc6ea4b1f5
Kushagra Gupta <guptakushagra15.10@gmail.com>
2022-02-13 21:15:25 +0530

    ✨ Add login page, favicons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "tailwindcss/tailwind.css";
+import Login from "./components/login";
+import Home from "./components/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-red-500"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/home" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
